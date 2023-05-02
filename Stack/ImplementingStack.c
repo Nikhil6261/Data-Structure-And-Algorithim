d235d0ea1f851e43dd599339a781b87edb1d140f
Nikhil6261 <nikkuahirwar2@gmail.com>
2023-05-02 17:05:34 +0530

    v1.0

--- Stack/ImplementingStack.c
+++ Stack/ImplementingStack.c
@@ -1,7 +1,27 @@
 #include<stdio.h>
-int main(){
+#define size 5
 
-    printf("hello");
+struct stack{
+    int arr[size];
+    int tos;
+};
+
+void push(struct stack*,int );
+int pop(stack stack*);
+
+int main(){
+struct stack s1; 
+push(&s1,10);
+
+printf(pop(s1));
     
     return 0;
 }
+void push(struct stack *p,int ele ){
+    if((*p)->tos==-1){
+        p->arr[p->tos]=ele;
+        printf("element added successfully");
+        
+    }
+}
+int pop(stack stack*);
