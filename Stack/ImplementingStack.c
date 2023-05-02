3d3bde1494c64c574dc9d4f202b1253560c21206
Nikhil6261 <nikkuahirwar2@gmail.com>
2023-05-02 20:42:59 +0530

    v1

--- Stack/ImplementingStack.c
+++ Stack/ImplementingStack.c
@@ -10,18 +10,33 @@ void push(struct stack*,int );
 int pop(stack stack*);
 
 int main(){
-struct stack s1; 
+struct stack s1;
+s1.tos=-1;
+
 push(&s1,10);
 
 printf(pop(s1));
     
     return 0;
 }
-void push(struct stack *p,int ele ){
-    if((*p)->tos==-1){
-        p->arr[p->tos]=ele;
-        printf("element added successfully");
-        
+void push(struct stack *ps,int ele ){
+    if((*ps)->tos==-1){
+       printf("Stack underflow");
+       return 0;
     }
+
+    ps->tos++;    
+    ps->arr[ps->tos]=ele;
+    printf("element added successfully %d",ele);
+
 }
-int pop(stack stack*);
+
+int pop(stack stack *ps){
+int x;
+
+if((ps)->tos == define) printf("Stack overflow");
+
+x=ps->arr[p->tos];
+ps->tos--;
+return x;
+}
