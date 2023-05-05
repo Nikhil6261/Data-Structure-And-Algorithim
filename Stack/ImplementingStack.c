d6df6d4b4aba903129719b2563e8662ef8240d21
Nikhil Ahirwar <130213827+Nikhil6261@users.noreply.github.com>
2023-05-05 14:00:04 +0530

    basic ImplementingStack.c
    basic

--- Stack/ImplementingStack.c
+++ Stack/ImplementingStack.c
@@ -2,41 +2,52 @@
 #define size 5
 
 struct stack{
-    int arr[size];
-    int tos;
+   int arr[size];
+   int tos;
 };
 
-void push(struct stack*,int );
-int pop(stack stack*);
+void push(struct stack* ,int );
+int pop(struct stack* );
 
 int main(){
 struct stack s1;
-s1.tos=-1;
+ s1.tos=-1;
 
-push(&s1,10);
+  push(&s1,10);
+  push(&s1,20);
+  push(&s1,30);
+  push(&s1,40);
+  push(&s1,50);
 
-printf(pop(s1));
-    
-    return 0;
+  int i;
+for(i=1;i<6;i++){
+
+     int x=pop(&s1);
+    printf("pop element is %d \n",x);
 }
+
+return 0;
+}
+
 void push(struct stack *ps,int ele ){
-    if((*ps)->tos==-1){
-       printf("Stack underflow");
-       return 0;
-    }
-
-    ps->tos++;    
+    if(ps->tos == 5){
+       printf("Stack overflow");
+       return ;
+ }
+    ps->tos++;
     ps->arr[ps->tos]=ele;
-    printf("element added successfully %d",ele);
-
+    printf("element added successfully %d \n",ele);
 }
 
-int pop(stack stack *ps){
+int pop(struct stack *ps){
 int x;
 
-if((ps)->tos == define) printf("Stack overflow");
+if(ps->tos == -1){
+    printf("Stack underflow");
+return -1;
+}
 
-x=ps->arr[p->tos];
+x=ps->arr[ps->tos];
 ps->tos--;
 return x;
 }
